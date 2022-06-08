97505af6348d6484cb2d92835320df3908494ddb
Alan Olimpio <alancleyton.olimpio@gmail.com>
2022-06-08 13:20:47 -0300

    feat: criando style global

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -1,1 +1,43 @@
-import {createGlobalStyle} from 'styled-components'
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background:#f0f2f5;
+    --red: #E52E4d;
+    --blue: #5429CC;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cn3;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+
+  html {
+    @media (max-width:1080px){
+      font-size:93.75%;
+    }
+    @media (max-width:729px){
+      font-size:87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing:antialiased;
+  }
+
+  button {
+    cursor:pointer;
+  }
+  [disabled]{
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+`
